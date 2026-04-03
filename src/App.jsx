98ecab4ad5e491987fd30d4a875ec4305ea0ec50
voucherinfinity <voucherinfinity9@gmail.com>
2026-04-03 15:26:44 +0530

    update admin file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Blogs from './components/Blogs'
 function App() {
   return (
-  <>
-  <Blogs />
+    <>
+        <Routes>
+        <Route path="/" element={<Blogs />} />
+          
+      </Routes>
+
   </>
   )
 }
